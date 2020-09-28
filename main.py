d1d0dedc6581d371c6c7848b90c5398d589a84ac
Vincent <vincentruelle.pro@gmail.com>
2020-09-28 16:34:25 +0200

    player class + move method

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ class Labyrinthe():
         self.player_x = ""
         self.player_y = ""
 
+        #Données des objets
+        
+
         #Chargement de la map à chaque instance
         self.loadMap()
 
@@ -19,19 +22,29 @@ class Labyrinthe():
                 for row, tiles in enumerate(self.map_data):
                     for col, tile in enumerate(tiles):
                         if tile == "1": # Mur
-                            self.map[f"{row}, {col}"] = "wall" 
+                            self.map[f"{row}, {col}"] = "mur" 
                         elif tile == ".": # Chemin
-                            self.map[f"{row}, {col}"] = "ground" 
-                        elif tile == "x": # Joueur
+                            self.map[f"{row}, {col}"] = "chemin" 
+                        elif tile == "X": # McGyver
                             self.map[f"{row}, {col}"] = "player" 
                             self.player_x = row
                             self.player_y = col
+                        elif tile == "G": # Gardien
+                            self.map[f"{row}, {col}"] = "gardien" 
+                        elif tile == "S": # Seringue
+                            self.map[f"{row},{col}"] = "seringue"
+                        elif tile == "E": # Ether
+                            self.map[f"{row},{col}"] = "ether"
+                        elif tile == "A": # Aiguille
+                            self.map[f"{row},{col}"] = "aiguille"
 
 class Player():
     def __init__(self):
         Map = Labyrinthe()
         self.x = Map.player_x
         self.y = Map.player_y
+
+        self.inventory = list()
     
     def movePlayer(self, key):
         if key == "z":
@@ -46,6 +59,9 @@ class Player():
         elif key == "d":
             self.x += 1
             print(self.x, self.y)
+
+    def addtoInventory(self, item):
+        pass
 
 player = Player()
 player.movePlayer("z")
